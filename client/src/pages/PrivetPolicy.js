334aab4eee675d8a22f6035b366acb52d6ff5ffe
Al Noor Istiak Mahmud <istiakmahmud1010@gmail.com>
2023-10-03 00:39:57 +0600

    adding footer

--- client/src/pages/PrivetPolicy.js
+++ client/src/pages/PrivetPolicy.js
@@ -3,7 +3,57 @@ import React from "react";
 const PrivetPolicy = () => {
   return (
     <>
-      <h1>PrivetPolicy</h1>
+      <div className="content-center py-10 bg-green-400">
+        <div>
+          <h2 className="text-center">Privacy & Policy</h2>
+        </div>
+        <div className="p-16">
+          <p className="p-5">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Praesentium corrupti recusandae eaque commodi maiores, rerum vitae
+            fuga non atque voluptates, quibusdam delectus officiis est rem qui
+            cumque, voluptas minus nemo. Expedita excepturi veniam odio unde
+            vitae, voluptates ipsum, error quae, iusto omnis aliquid odit nam.
+            Tempora facere, sit maiores pariatur a fugiat ratione dolores,
+            molestias ab illo necessitatibus adipisci sunt, voluptates molestiae
+            possimus quae in nulla impedit error nihil at excepturi
+            reprehenderit dolore corrupti? Dolores dolore molestias quae
+            laboriosam, iure, dicta odit odio amet, quo repellendus minus! At
+            obcaecati eveniet veritatis, reiciendis, odio, velit corporis
+            mollitia provident eligendi voluptas ratione.
+          </p>
+
+          <p className="p-5">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Praesentium corrupti recusandae eaque commodi maiores, rerum vitae
+            fuga non atque voluptates, quibusdam delectus officiis est rem qui
+            cumque, voluptas minus nemo. Expedita excepturi veniam odio unde
+            vitae, voluptates ipsum, error quae, iusto omnis aliquid odit nam.
+            Tempora facere, sit maiores pariatur a fugiat ratione dolores,
+            molestias ab illo necessitatibus adipisci sunt, voluptates molestiae
+            possimus quae in nulla impedit error nihil at excepturi
+            reprehenderit dolore corrupti? Dolores dolore molestias quae
+            laboriosam, iure, dicta odit odio amet, quo repellendus minus! At
+            obcaecati eveniet veritatis, reiciendis, odio, velit corporis
+            mollitia provident eligendi voluptas ratione.
+          </p>
+
+          <p className="p-5">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Praesentium corrupti recusandae eaque commodi maiores, rerum vitae
+            fuga non atque voluptates, quibusdam delectus officiis est rem qui
+            cumque, voluptas minus nemo. Expedita excepturi veniam odio unde
+            vitae, voluptates ipsum, error quae, iusto omnis aliquid odit nam.
+            Tempora facere, sit maiores pariatur a fugiat ratione dolores,
+            molestias ab illo necessitatibus adipisci sunt, voluptates molestiae
+            possimus quae in nulla impedit error nihil at excepturi
+            reprehenderit dolore corrupti? Dolores dolore molestias quae
+            laboriosam, iure, dicta odit odio amet, quo repellendus minus! At
+            obcaecati eveniet veritatis, reiciendis, odio, velit corporis
+            mollitia provident eligendi voluptas ratione.
+          </p>
+        </div>
+      </div>
     </>
   );
 };
