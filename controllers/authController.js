f10edd2d7b9cd63801e698482cb34f412d8e2c19
Al Noor Istiak Mahmud <istiakmahmud1010@gmail.com>
2023-10-02 19:52:03 +0600

    registration user auth setting

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,54 @@
-export const registerController = async (req, res) => {};
+import { hashPassword } from "../helpers/authHelper.js";
+import userModel from "../models/userModels.js";
+
+export const registerController = async (req, res) => {
+  try {
+    const { name, email, password, phone, address } = req.body;
+    if (!name) {
+      return res.send({ error: "Name is required" });
+    }
+    if (!email) {
+      return res.send({ error: "email is required" });
+    }
+    if (!password) {
+      return res.send({ error: "password is required" });
+    }
+    if (!phone) {
+      return res.send({ error: "phone is required" });
+    }
+    if (!address) {
+      return res.send({ error: "address is required" });
+    }
+    // chack user
+    const existingUser = await userModel.findOne({ email });
+    // existing user
+    if (existingUser) {
+      return res.status(200).send({
+        success: true,
+        message: "already registered Please login",
+      });
+    }
+    // register user
+    const hashedPassword = await hashPassword(password);
+    // save
+    const user = await new userModel({
+      name,
+      email,
+      phone,
+      address,
+      password: hashedPassword,
+    }).save();
+    res.status(201).send({
+      success: true,
+      message: "User Registre successfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "error in registation",
+      error,
+    });
+  }
+};
