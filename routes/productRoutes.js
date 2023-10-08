c5d5c6449e2fb41ce563d94304e5c55f5a5d6bab
Al Noor Istiak Mahmud <istiakmahmud1010@gmail.com>
2023-10-08 13:33:18 +0600

    adding product api crud operation in database

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,16 +1,45 @@
 import express from "express";
 import { isAdmin, requireSignIn } from "../middlewares/authMiddlewares.js";
-import { createProduct } from "../controllers/productController.js";
+import {
+  createProductController,
+  getProductController,
+  getSingleProductController,
+  productPhotoController,
+  deleteProductController,
+  updateProductController,
+} from "../controllers/productController.js";
 import formidable from "express-formidable";
 
 const router = express.Router();
-
+// rutes
+// create product
 router.post(
   "/create-product",
   requireSignIn,
   isAdmin,
   formidable(),
-  createProduct
+  createProductController
+);
+
+// get product
+router.get("/get-product", getProductController);
+
+// get single product
+router.get("/get-product/:slug", getSingleProductController);
+
+// get product photo
+router.get("/product-photo/:pid", productPhotoController);
+
+// delete product
+router.delete("/product/:pid", deleteProductController);
+
+// update product
+router.put(
+  "/update-product/:pid",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  updateProductController
 );
 
 export default router;
